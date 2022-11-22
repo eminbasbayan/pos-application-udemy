83331b217f4c607ed3a533be637fb0ca3cb0d422
eminbasbayan <emin.basbayan@icloud.com>
2022-11-22 20:24:50 +0300

    genel proje ayarları

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ function App() {
   return (
     <div>
       <Button type="primary">Primary Button</Button>
+      <Button type="primary" danger>
+      Primary
+    </Button>
       <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
     </div>
   );
