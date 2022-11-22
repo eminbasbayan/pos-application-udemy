fb0d16aef7cbf15af69bb776439eb8166905d1f9
eminbasbayan <emin.basbayan@icloud.com>
2022-11-22 20:10:33 +0300

    tailwindcss kurulumu

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div>
       <Button type="primary">Primary Button</Button>
+      <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
     </div>
   );
 }
