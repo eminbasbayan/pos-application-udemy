b68b9b13bf1604ad980cbc707d48ddd1df73f3b3
eminbasbayan <emin.basbayan@icloud.com>
2022-11-22 23:52:16 +0300

    home page kurulum

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import React from "react";
 import Header from "./components/header/Header";
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      home
-    </div>
+      <div className="home px-6 flex justify-between">
+        <div className="categories">
+          <div>categories</div>
+        </div>
+        <div className="products">
+          <div className="">products</div>
+        </div>
+        <div className="">
+          <div>cart totals</div>
+        </div>
+      </div>
+    </>
   );
 }
 
