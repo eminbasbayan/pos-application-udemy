5c71bd68753bd80221b25991ecc0da5c9af40c64
eminbasbayan <emin.basbayan@icloud.com>
2022-11-23 22:09:57 +0300

    istatistik card

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -6,11 +6,24 @@ const StatisticPage = () => {
       <Header />
       <div className="px-6">
         <h1 className="text-4xl font-bold text-center mb-4">İstatistiklerim</h1>
-        <div>
+        <div className="statistic-section">
           <h2 className="text-lg">
             Hoş geldin{" "}
             <span className="text-green-700 font-bold text-xl">admin</span>.
           </h2>
+          <div className="statistic-cards grid grid-cols-4 my-10">
+            <div className="card-item bg-gray-800 p-8 rounded-lg">
+              <div className="flex gap-x-4">
+                <div className="rounded-full bg-white w-16 h-16 p-3">
+                  <img src="images/user.png" alt="" />
+                </div>
+                <div className="text-white">
+                  <p className="mb-2 text-lg font-medium text-gray-400">Toplam Müşteri</p>
+                  <p className="text-xl font-semibold text-gray-200">6</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
