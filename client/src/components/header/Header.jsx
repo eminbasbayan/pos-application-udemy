a862bd2f17b67619b79b6fec904df2a8a25ffbf9
eminbasbayan <emin.basbayan@icloud.com>
2022-11-23 14:13:26 +0300

    link  kullanımı

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Badge, Input } from "antd";
 import {
   SearchOutlined,
@@ -8,14 +9,15 @@ import {
   BarChartOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
+
 const Header = () => {
   return (
     <div className="border-b mb-6">
       <header className="py-4 px-6 flex justify-between items-center gap-10">
         <div className="logo">
-          <a href="/">
+          <Link to="/">
             <h2 className="text-2xl font-bold md:text-4xl">LOGO</h2>
-          </a>
+          </Link>
         </div>
         <div className="header-search flex-1 flex justify-center">
           <Input
@@ -26,60 +28,60 @@ const Header = () => {
           />
         </div>
         <div className="menu-links flex justify-between items-center gap-7 md:static fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
-          <a
-            href={"/"}
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <HomeOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Ana Sayfa</span>
-          </a>
-          <Badge count={5} offset={[0,6]} className="md:flex hidden">
-            <a
-              href={"/"}
+          </Link>
+          <Badge count={5} offset={[0, 6]} className="md:flex hidden">
+            <Link
+              to={"/cart"}
               className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
             >
               <ShoppingCartOutlined className="md:text-2xl text-xl" />
               <span className="md:text-xs text-[10px]">Sepet</span>
-            </a>
+            </Link>
           </Badge>
-          <a
-            href={"/"}
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <CopyOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Faturalar</span>
-          </a>
-          <a
-            href={"/"}
+          </Link>
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Müşteriler</span>
-          </a>
-          <a
-            href={"/"}
+          </Link>
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">İstatistikler</span>
-          </a>
-          <a
-            href={"/"}
+          </Link>
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <LogoutOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Çıkış</span>
-          </a>
+          </Link>
         </div>
-        <Badge count={5} offset={[0,6]} className="md:hidden flex">
-            <a
-              href={"/"}
-              className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
-            >
-              <ShoppingCartOutlined className="text-2xl" />
-              <span className="md:text-xs text-[10px]">Sepet</span>
-            </a>
-          </Badge>
+        <Badge count={5} offset={[0, 6]} className="md:hidden flex">
+          <Link
+            to={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <ShoppingCartOutlined className="text-2xl" />
+            <span className="md:text-xs text-[10px]">Sepet</span>
+          </Link>
+        </Badge>
       </header>
     </div>
   );
