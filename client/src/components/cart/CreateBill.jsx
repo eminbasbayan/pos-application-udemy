a7865d83d0ff42ef7fd6423b813eceb15329a0b9
eminbasbayan <emin.basbayan@icloud.com>
2022-11-23 12:28:47 +0300

    cart page form

--- client/src/components/cart/CreateBill.jsx
+++ client/src/components/cart/CreateBill.jsx
@@ -1,6 +1,10 @@
-import { Modal } from "antd";
+import { Button, Card, Form, Input, Modal, Select } from "antd";
 
 const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
     <Modal
       title="Fatura Oluştur"
@@ -8,9 +12,61 @@ const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
       footer={false}
       onCancel={() => setIsModalOpen(false)}
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <Form layout={"vertical"} onFinish={onFinish}>
+        <Form.Item
+          label="Müşteri Adı"
+          name={"curtomerName"}
+          rules={[
+            {
+              required: true,
+              message: "Username is required",
+            },
+          ]}
+        >
+          <Input placeholder="Bir Müşteri Adı Yazınız" />
+        </Form.Item>
+        <Form.Item
+          rules={[{ required: true }]}
+          name={"phoneNumber"}
+          label="Tel No"
+        >
+          <Input placeholder="Bir Tel No Yazınız" maxLength={11} />
+        </Form.Item>
+        <Form.Item
+          label="Ödeme Yöntemi"
+          rules={[{ required: true }]}
+          name={"paymentMode"}
+        >
+          <Select placeholder="Bir Ödeme Yöntemi Seçiniz">
+            <Select.Option value="Nakit">Nakit</Select.Option>
+            <Select.Option value="Kredi Kartı">Kredi Kartı</Select.Option>
+          </Select>
+        </Form.Item>
+        <Card>
+          <div className="flex justify-between">
+            <span>Ara Toplam</span>
+            <span>549.00₺</span>
+          </div>
+          <div className="flex justify-between my-2">
+            <span>KDV Toplam %8</span>
+            <span className="text-red-600">+43.92₺</span>
+          </div>
+          <div className="flex justify-between">
+            <b>Toplam</b>
+            <b>592.92₺</b>
+          </div>
+          <div className="flex justify-end">
+            <Button
+              className="mt-4"
+              type="primary"
+              onClick={() => setIsModalOpen(true)}
+              htmlType="submit"
+            >
+              Sipariş Oluştur
+            </Button>
+          </div>
+        </Card>
+      </Form>
     </Modal>
   );
 };
