ca4f10bf17bb0dbb1c66fab1f5caf23e1d92e271
eminbasbayan <emin.basbayan@icloud.com>
2022-11-24 23:10:26 +0300

    product schema oluşturma

---
+++ api/models/Product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    title: { tpye: String, require: true },
+    img: { type: String, require: true },
+    price: { type: Number, require: true },
+    category: { type: String, require: true },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("products", ProductSchema);
+module.exports = Product;
