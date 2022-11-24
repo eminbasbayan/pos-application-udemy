68332a5289e002f8e85b7e20e4ccb81719d92cae
eminbasbayan <emin.basbayan@icloud.com>
2022-11-24 22:35:55 +0300

    mongodb kurulumu

--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 const port = 5000;
 
-app.get("/", (req,res)=> res.send("Hello World!"))
+dotenv.config(); 
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => {
+  connect();
   console.log(`Example app listening on port ${port}`);
 });
- 
