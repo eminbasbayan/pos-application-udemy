bbd796f4abf195dbea577a4cca53238c48e903aa
eminbasbayan <emin.basbayan@icloud.com>
2022-11-24 11:31:44 +0300

    server-express kurulumu

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require("express");
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req,res)=> res.send("Hello World!"))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+ 
