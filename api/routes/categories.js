b8cb5d790a0e0ace4c4b71f80058247ad7d3a479
eminbasbayan <emin.basbayan@icloud.com>
2022-11-27 14:21:10 +0300

    category CRUD işlemleri

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -2,6 +2,17 @@ const Category = require("../models/Category.js");
 const express = require("express");
 const router = express.Router();
 
+//! get all category
+router.get("/get-all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//! create
 router.post("/add-category", async (req, res) => {
   try {
     const newCategory = new Category(req.body);
@@ -9,6 +20,26 @@ router.post("/add-category", async (req, res) => {
     res.status(200).json("Item added successfully.");
   } catch (error) {
     res.status(400).json(error);
+  }
+});
+
+//! update
+router.put("/update-category", async (req, res) => {
+  try {
+    await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body);
+    res.status(200).json("Item updated successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//! delete
+router.delete("/delete-category", async (req, res) => {
+  try {
+    await Category.findOneAndDelete({ _id: req.body.categoryId });
+    res.status(200).json("Item deleted successfully.");
+  } catch (error) {
+    console.log(error);
   }
 });
 
