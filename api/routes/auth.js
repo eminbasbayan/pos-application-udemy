f9a4aff6b1410735026c374575db1a5a3ff90e95
eminbasbayan <emin.basbayan@icloud.com>
2022-11-27 15:18:23 +0300

    güvenli parola oluşturma

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,16 +1,23 @@
 const User = require("../models/User.js");
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
 //! register
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const { username, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
     await newUser.save();
     res.status(200).json("A new user created successfully.");
   } catch (error) {
     res.status(400).json(error);
   }
 });
-
 
 module.exports = router;
