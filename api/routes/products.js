3c70c7dd556f68dae626893fd84fef64fd5b02d5
eminbasbayan <emin.basbayan@icloud.com>
2022-11-27 14:35:03 +0300

    product CRUD işlemleri

---
+++ api/routes/products.js
@@ -0,0 +1,46 @@
+const Product = require("../models/Product.js");
+const express = require("express");
+const router = express.Router();
+
+//! get all Product
+router.get("/get-all", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//! create
+router.post("/add-product", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(200).json("Item added successfully.");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//! update
+router.put("/update-product", async (req, res) => {
+  try {
+    await Product.findOneAndUpdate({ _id: req.body.productId }, req.body);
+    res.status(200).json("Item updated successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//! delete
+router.delete("/delete-product", async (req, res) => {
+  try {
+    await Product.findOneAndDelete({ _id: req.body.productId });
+    res.status(200).json("Item deleted successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const port = 5000;
 
 // routes
 const categoryRoute = require("./routes/categories.js");
+const productRoute = require("./routes/products.js");
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/categories", categoryRoute);
+app.use("/api/products", productRoute);
 
 app.listen(port, () => {
   connect();
