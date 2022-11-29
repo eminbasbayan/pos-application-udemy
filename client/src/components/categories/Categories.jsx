df788133fde1b907c7d45eaf7306e73a719d2005
eminbasbayan <emin.basbayan@icloud.com>
2022-11-29 13:03:27 +0300

    kategori düzenleme

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -36,6 +36,8 @@ const Categories = ({ categories, setCategories }) => {
       <Edit
         isEditModalOpen={isEditModalOpen}
         setIsEditModalOpen={setIsEditModalOpen}
+        categories={categories}
+        setCategories={setCategories}
       />
     </ul>
   );
--- client/src/components/categories/Edit.jsx
+++ client/src/components/categories/Edit.jsx
@@ -1,7 +1,74 @@
-import { Form, Modal, Table } from "antd";
-import React from "react";
+import { Button, Form, Input, message, Modal, Table } from "antd";
+import React, { useState } from "react";
 
-const Edit = ({ isEditModalOpen, setIsEditModalOpen }) => {
+const Edit = ({
+  isEditModalOpen,
+  setIsEditModalOpen,
+  categories,
+  setCategories,
+}) => {
+  const [editingRow, setEditingRow] = useState({});
+
+  const onFinish = (values) => {
+    console.log(values);
+    try {
+      fetch("http://localhost:5000/api/categories/update-category", {
+        method: "PUT",
+        body: JSON.stringify({ ...values, categoryId: editingRow._id }),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      message.success("Kategori başarıyla güncellendi.");
+      setCategories(
+        categories.map((item) => {
+          if (item._id === editingRow._id) {
+            return { ...item, title: values.title };
+          }
+          return item;
+        })
+      );
+    } catch (error) {
+      message.success("Bir şeyler yanlış gitti.");
+      console.log(error);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Category Title",
+      dataIndex: "title",
+      render: (_, record) => {
+        if (record._id === editingRow._id) {
+          return (
+            <Form.Item className="mb-0" name="title">
+              <Input defaultValue={record.title} />
+            </Form.Item>
+          );
+        } else {
+          return <p>{record.title}</p>;
+        }
+      },
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: (text, record) => {
+        return (
+          <div>
+            <Button type="link" onClick={() => setEditingRow(record)} className="pl-0">
+              Düzenle
+            </Button>
+            <Button type="link" htmlType="submit" className="text-gray-500">
+              Kaydet
+            </Button>
+            <Button type="link" danger>
+              Sil
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
     <Modal
       open={isEditModalOpen}
@@ -9,8 +76,13 @@ const Edit = ({ isEditModalOpen, setIsEditModalOpen }) => {
       footer={false}
       onCancel={() => setIsEditModalOpen(false)}
     >
-      <Form>
-        <Table bordered />
+      <Form onFinish={onFinish}>
+        <Table
+          bordered
+          dataSource={categories}
+          columns={columns}
+          rowKey={"_id"}
+        />
       </Form>
     </Modal>
   );
