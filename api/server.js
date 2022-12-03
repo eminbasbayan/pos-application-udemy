c0fc1f5f4875641617a8d8ba4fcc44e96225ad23
eminbasbayan <emin.basbayan@icloud.com>
2022-12-03 22:17:00 +0300

    server .env güncelleme

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const app = express();
 const cors = require("cors");
 const logger = require("morgan");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // routes
 const categoryRoute = require("./routes/categories.js");
